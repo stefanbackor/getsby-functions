f6a08bd343da4f157c0b7e3ce38e455e64a76cd9
Štefan Bačkor <stevo@backor.sk>
2020-03-12 11:17:32 +0100

    Update FWNTest.js

--- src/FWNTest.js
+++ src/FWNTest.js
@@ -5,6 +5,23 @@ class HomeContainer extends React.Component {
     super(props);
     this.renderAds = this.renderAds.bind(this);
   }
+  
+  componentDidMount() {
+    window && window.eval && window.eval(`
+      !function(e,t,c,a){if(!e.fwn&&(a="fwn_script",n=e.fwn=function(){
+      n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)
+      },e._fwn||(e._fwn=n),n.queue=[],!t.getElementById(a))){var d=document.createElement("script");
+      d.async=1,d.src=c,d.id=a,t.getElementsByTagName("head")[0].appendChild(d)}
+      }(window,document,"//asset.fireworktv.com/js/fwn.js");
+
+      fwn('app_id', 'Hv4b8ZJ3VjxHMg2H9zBEQyYn79CZZ3Zp');
+      fwn('mode', 'row'); // 'grid' or 'row'
+      fwn('placement', 'middle'); // 'top', 'middle', 'bottom', or 'fullscreen'
+      fwn('max_videos', '12'); // set 0 for infinite scroll
+      fwn('page_type', 'feed');
+      fwn('target', document.getElementById('fwn_videos'));
+    `)
+  }
 
   renderAds() {
     return (
@@ -37,7 +54,10 @@ class HomeContainer extends React.Component {
     return (
       <div className="home-container page-container">
         <div className="center-container">
-          {this.renderAds()}
+      
+          <div id="fwn_videos" />
+      
+          {/* this.renderAds() */}
           {/* {this.renderTrendingGames()} */}
         </div>
       </div>
